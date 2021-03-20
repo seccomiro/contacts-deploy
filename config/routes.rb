5d06746c5cc9719b36f5195957a869e7fa450d7a
Diego Stiehl <seccomiro@users.noreply.github.com>
2021-03-19 22:12:49 -0300

    Create Contact scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :contacts
   resources :users
   resources :companies
   resources :kinds
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_19_232317) do
+ActiveRecord::Schema.define(version: 2021_03_20_010233) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
@@ -19,8 +19,24 @@ ActiveRecord::Schema.define(version: 2021_03_19_232317) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "kinds", force: :cascade do |t|
     t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tests", force: :cascade do |t|
+    t.string "name"
+    t.date "birthday"
+    t.integer "age"
+    t.boolean "active"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
