ca54c289211b3b2a1eae06ad136791c2cc46e545
Diego Stiehl <seccomiro@users.noreply.github.com>
2021-03-29 18:54:08 -0300

    Add has_many :through associations

--- app/models/kind.rb
+++ app/models/kind.rb
@@ -1,5 +1,8 @@
 class Kind < ApplicationRecord
   has_many :contacts
+  # # OPTIONAL
+  # has_many :phones, through: :contacts
+  # has_many :addresses, through: :contacts
 
   def to_s
     description
