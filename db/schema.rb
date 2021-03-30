658e7d2b970ca70111b295b096e0a428830a7e9e
Diego Stiehl <seccomiro@users.noreply.github.com>
2021-03-29 22:36:26 -0300

    Add Devise to the project

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_29_220643) do
+ActiveRecord::Schema.define(version: 2021_03_30_012607) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "street"
@@ -62,10 +62,15 @@ ActiveRecord::Schema.define(version: 2021_03_29_220643) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "email"
-    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "addresses", "contacts"
