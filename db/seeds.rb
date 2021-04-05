a94e833838699c26dad2622d3effd808467f4a0c
Diego Stiehl <seccomiro@users.noreply.github.com>
2021-04-05 12:30:48 -0300

    Create seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+  Kind.create(description: Faker::Job.unique.field)
+end
+
+8.times do
+  Company.create(name: Faker::Company.unique.name, active: Faker::Boolean.boolean)
+end
+
+10.times do
+  User.create(
+    email: Faker::Internet.unique.email,
+    name: Faker::Movies::StarWars.unique.character,
+    password: '321321321',
+    password_confirmation: '321321321'
+  )
+end
+
+100.times do
+  contact = Contact.create(
+    email: Faker::Internet.unique.email,
+    name: Faker::Name.unique.name_with_middle,
+    remark: Faker::Lorem.paragraph,
+    kind: Kind.all.sample,
+    company: Company.all.sample
+  )
+
+  user_count = (1..10).to_a.sample
+  User.all.sample(user_count).each do |user|
+    contact.users << user
+  end
+
+  (0..5).to_a.sample.times do
+    contact.phones.create(number: Faker::PhoneNumber.unique.cell_phone)
+  end
+
+  contact.create_address(
+    city: Faker::Address.city,
+    state: Faker::Address.state,
+    street: Faker::Address.street_address
+  ) if Faker::Boolean.boolean
+end
